efc5c0fdd1290808363abcac48b737685090d700
cclutta <52520502+cclutta@users.noreply.github.com>
2022-12-24 08:04:23 +0300

    Create 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,36 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  
+  get name() {
+    return this._name
+  }
+  
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('Name must be a string');
+    this._name = newName;
+  }
+  
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw TypeError('Length must be a number');
+    this._length = newLength;
+  }
+  
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!(newStudents instanceof Array)) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
