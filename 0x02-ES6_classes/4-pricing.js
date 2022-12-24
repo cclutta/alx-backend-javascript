ad6eec47582b4654d08dfdd1542713ca9bbcef7d
cclutta <52520502+cclutta@users.noreply.github.com>
2022-12-24 08:32:06 +0300

    Create 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,20 @@
+import Currency from '3-currency.js'
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    
+  }
+}
