3e8d5f6e6ed7e651cf3a89e9d1f03bdae6533eb5
cclutta <chelsea.lutta@gmail.com>
2023-02-21 19:17:48 +0300

    Update

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,12 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-
-const host = 'localhost'
-const port = 1245
+const app = express();
+const host = 'localhost';
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 
 app.listen(port, host);
 
